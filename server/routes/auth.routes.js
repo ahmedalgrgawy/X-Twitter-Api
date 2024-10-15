c2e6c9a12e6fb1ac743d09307a63851a64fd1ef7
ahmedalgrgawy <ahmedalgrgawy77@gmail.com>
2024-10-15 18:08:11 +0300

    Authentication & Authorization

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,8 +1,17 @@
 import express from 'express'
-import { login } from '../controllers/auth.controller.js'
+import { getProfile, login, logout, reCreateAccessToken, signup } from '../controllers/auth.controller.js'
+import { protectedRoute } from '../middleware/auth.middleware.js'
 
 const router = express.Router()
 
-router.get('/login', login)
+router.post("/signup", signup)
+
+router.post("/login", login)
+
+router.post("/logout", logout)
+
+router.post("/recreate-token", reCreateAccessToken)
+
+router.get("/me", protectedRoute, getProfile)
 
 export default router
