ef432ad13a8d1e39bd1af283d1c779ad65f63db1
ahmedalgrgawy <ahmedalgrgawy77@gmail.com>
2024-10-16 20:58:39 +0300

    Update Profile

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,3 +1,4 @@
+import cloudinary from "../lib/cloudinary.js";
 import Notification from "../models/notification.model.js";
 import User from "../models/user.model.js";
 
@@ -121,4 +122,73 @@ export const toggleFollow = async (req, res) => {
     }
 }
 
-export const updateProfile = async (req, res) => { }
+export const updateProfile = async (req, res) => {
+
+    const userId = req.user._id;
+    const { fullName, username, email, currentPassword, newPassword, bio, link } = req.body;
+    let { profileImg, coverImg } = req.body;
+
+    try {
+
+        let user = await User.findById(userId);
+
+        if (!user) {
+            return res.status(404).json({ success: false, message: "User not found" });
+        }
+
+        if ((!newPassword && currentPassword) || (!currentPassword && newPassword)) {
+            return res.status(400).json({ error: "Please provide both current password and new password" });
+        }
+
+        if (currentPassword && newPassword) {
+            const isMatch = await user.comparePassword(currentPassword);
+
+            if (!isMatch) {
+                return res.status(400).json({ error: "Current password is incorrect" });
+            }
+
+            if (newPassword.length < 6) {
+                return res.status(400).json({ error: "Password must be at least 6 characters long" });
+            }
+
+            user.password = newPassword;
+        }
+
+        if (profileImg) {
+            if (user.profileImg) {
+                await cloudinary.uploader.destroy(user.profileImg.split("/").pop().split(".")[0]);
+            }
+
+            const uploadedResponse = await cloudinary.uploader.upload(profileImg);
+            profileImg = uploadedResponse.secure_url;
+        }
+
+        if (coverImg) {
+            if (user.coverImg) {
+                await cloudinary.uploader.destroy(user.coverImg.split("/").pop().split(".")[0]);
+            }
+
+            const uploadedResponse = await cloudinary.uploader.upload(coverImg);
+            coverImg = uploadedResponse.secure_url;
+        }
+
+        user.fullName = fullName || user.fullName;
+        user.email = email || user.email;
+        user.username = username || user.username;
+        user.bio = bio || user.bio;
+        user.link = link || user.link;
+        user.profileImg = profileImg || user.profileImg;
+        user.coverImg = coverImg || user.coverImg;
+
+        user = await user.save();
+
+        user.password = null;
+
+        return res.status(200).json({ success: true, message: "Profile updated successfully", user });
+
+    } catch (error) {
+        console.log(error);
+
+        return res.status(500).json({ success: false, message: "Server Error", error: error.message });
+    }
+}
