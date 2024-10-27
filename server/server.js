5eb9ff74c4de744334ddcab053e84536fbbce2f1
ahmedalgrgawy <ahmedalgrgawy77@gmail.com>
2024-10-27 21:32:23 +0300

    Fixing

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ import express from 'express'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser';
 import cors from "cors";
-import path from 'path'
 import { connectDb } from './lib/connectDb.js';
 import authRoutes from './routes/auth.routes.js'
 import userRoutes from './routes/user.routes.js'
@@ -13,8 +12,6 @@ dotenv.config()
 
 const app = express();
 const port = process.env.PORT || 5000;
-
-const __dirname = path.resolve()
 
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }))
 
@@ -29,16 +26,6 @@ app.use("/api/auth", authRoutes)
 app.use("/api/user", userRoutes)
 app.use("/api/posts", postsRoutes)
 app.use("/api/notifications", notificationsRoutes)
-
-
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '/client/dist')));
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
-    });
-
-}
 
 app.listen(port, () => {
     console.log('Server Running on ' + port)
