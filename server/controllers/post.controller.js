36d3bd0621afc4c19d42023e267d14ccb3cd3ad5
ahmedalgrgawy <ahmedalgrgawy77@gmail.com>
2024-10-27 21:56:40 +0300

    Edit Post

--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -215,6 +215,36 @@ export const commentOnPost = async (req, res) => {
     }
 }
 
+export const editPost = async (req, res) => {
+    try {
+
+        const { text } = req.body;
+        let { img } = req.body;
+        const userId = req.user._id;
+        const post = await Post.findById(req.params.id);
+
+        if (!post) {
+            return res.status(404).json({ success: false, message: "Post not found" });
+        }
+
+        if (post.user.toString() !== userId.toString()) {
+            return res.status(401).json({ success: false, message: "Unauthorized" });
+        }
+
+        if (img) {
+            const uploadedResponse = await cloudinary.uploader.upload(img);
+            img = uploadedResponse.secure_url;
+        }
+
+        await Post.findByIdAndUpdate(req.params.id, { text, img }, { new: true });
+
+        return res.status(200).json({ success: true, message: "Post updated successfully" });
+
+    } catch (error) {
+        return res.status(500).json({ success: false, message: "Server Error", error: error.message });
+    }
+}
+
 export const deletePost = async (req, res) => {
     try {
 
--- server/routes/posts.routes.js
+++ server/routes/posts.routes.js
@@ -5,6 +5,7 @@ import {
     commentOnPost,
     createPost,
     deletePost,
+    editPost,
     getAllPosts,
     getFollowingPosts,
     getLikedPosts,
@@ -21,6 +22,7 @@ router.get("/user/:username", protectedRoute, getUserPosts);
 router.post("/create", protectedRoute, createPost);
 router.post("/like/:id", protectedRoute, toggleLikes);
 router.post("/comment/:id", protectedRoute, commentOnPost);
+router.post("/edit/:id", protectedRoute, editPost);
 router.delete("/delete/:id", protectedRoute, deletePost);
 
 export default router;
